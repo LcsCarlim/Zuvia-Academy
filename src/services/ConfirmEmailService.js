f537ea0a1b5825d6a1cd585d847c5a657403c7b6
Lucas <lucascarlim0520@gmail.com>
2023-06-06 12:43:54 -0300

    fix: code getting others users

--- src/services/ConfirmEmailService.js
+++ src/services/ConfirmEmailService.js
@@ -1,13 +1,12 @@
 const UserModel = require('../database/model/UserModel');
 
 module.exports = async ({ id, code }) => {
-  const userId = await UserModel.findById(id);
-  if (!userId) throw new Error('User not found!');
+  const user = await UserModel.findById(id);
+  if (!user) {
+    throw new Error('User not found');
+  }
 
-  const user = await UserModel.findOne({
-    code: {
-      code
-    }
-  });
-  if (!user) throw new Error('Invalid code!');
+  if (user.code.code !== code) {
+    throw new Error('Invalid code');
+  }
 };
