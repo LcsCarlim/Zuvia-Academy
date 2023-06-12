20c7101fac25706f6c72320e92b8bfa14e1b8379
Lucas <lucascarlim0520@gmail.com>
2023-06-12 16:37:56 -0300

    fix: confirm email

--- src/controller/ConfirmEmailController.js
+++ src/controller/ConfirmEmailController.js
@@ -1,16 +1,17 @@
 const ConfirmEmailService = require('../services/ConfirmEmailService');
 
 module.exports = async (req, res) => {
-  const { _id } = req.user;
-  const { code } = req.body;
   try {
-    const user = await ConfirmEmailService({ id: _id, code });
-    res.status(200).json({
+    const { user } = req;
+    const { code } = req.body;
+
+    const userConfirmed = await ConfirmEmailService({ user, code });
+    return res.status(200).json({
       message: 'Validated code',
-      user
+      user: userConfirmed
     });
   } catch (error) {
-    res.status(401).json({
+    return res.status(401).json({
       error: 'Something wrong happened, try again',
       message: error.message
     });
--- src/services/ConfirmEmailService.js
+++ src/services/ConfirmEmailService.js
@@ -1,16 +1,10 @@
 const UserModel = require('../database/model/UserModel');
 
-module.exports = async ({ id, code }) => {
-  const user = await UserModel.findById(id);
-
-  if (user.code.code !== code) {
+module.exports = async ({ user, code }) => {
+  // eslint-disable-next-line eqeqeq
+  if (user.code.code != code) {
     throw new Error('Invalid code');
   }
 
-  user.code.status = 'Used';
-  user.status = 'Approved';
-
-  console.log(await user.save());
-
-  await user.save();
+  return await UserModel.findOneAndUpdate({ _id: user._id }, { code: { ...user.code, status: 'Used' }, status: 'Approved' });
 };
